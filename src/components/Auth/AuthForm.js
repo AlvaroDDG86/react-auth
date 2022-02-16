fff4a038be7702b45c3e422d9386e3767988d40b
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-02-16 11:17:52 +0100

    Implement signup

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,36 +1,97 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const emailRef = useRef()
+  const passwordRef = useRef()
   const [isLogin, setIsLogin] = useState(true);
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault()
+    const emailVal = emailRef.current.value
+    const passwordVal = passwordRef.current.value
+    setIsLoading(true)
+    if (isLogin) {
+      // fetch('https://identitytoolkit.googleapis.com/v1/accounts:login?key=AIzaSyAliawKwwVuJ5J_71cWp30PJ5wy-0XTmsE', {
+      //   method: 'POST',
+      //   body: JSON.stringify({
+      //     email: emailVal,
+      //     password: passwordVal
+      //   }),
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   }
+      // }).then(async res => {
+      //   const data = await res.json()
+      //   if (res.ok) {
+      //     console.log(data)
+      //   } else {
+      //     setError(data.error.message)
+      //   }
+      // }).catch((error) => {
+      //   setError(error.message)
+      // }).finally(() => {
+      //   setIsLoading(false)
+      // })
+    } else {
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAliawKwwVuJ5J_71cWp30PJ5wy-0XTmsE', {
+        method: 'POST',
+        body: JSON.stringify({
+          email: emailVal,
+          password: passwordVal,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(async res => {
+        const data = await res.json()
+        if (res.ok) {
+          console.log(data)
+        } else {
+          setError(data.error.message)
+        }
+      }).catch((error) => {
+        setError(error.message)
+      }).finally(() => {
+        setIsLoading(false)
+      })
+    }
+  }
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={passwordRef} />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
+          { isLoading && <p className={classes.loading}>Loading...</p> }
+          { !isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button> }
+            <button
+              type='button'
+              className={classes.toggle}
+              onClick={switchAuthModeHandler}
+            >
+              {isLogin ? 'Create new account' : 'Login with existing account'}
+            </button>
+            
         </div>
+        { error && <p className={classes.error}>{ error }</p>}
       </form>
     </section>
   );
